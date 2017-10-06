8a6faa7ead46abb5e2dd9fd980a85ed4eecc8111
Criollo01 <lucanagy@protonmail.com>
2017-10-06 12:58:55 +0200

    feat(test): forgot about test writing, so couldn't finish it at all

---
+++ uniquechars/test_uniquechars.py
@@ -0,0 +1,7 @@
+import unittest
+import unique_chars
+
+class UniqueTestCases(unittest.TestCase):
+
+    def test_unique_chars_with_no_unique(self):
+        self.assertEqual(test_unique_chars_with_no_unique.unique_character())
