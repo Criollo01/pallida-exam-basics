6ccce37996bc4014894db5517d0f36393f5fddd8
Criollo01 <lucanagy@protonmail.com>
2017-10-06 12:04:26 +0200

    feat(fav_animals): print usage works

--- favouriteanimals/favourite_animals.py
+++ favouriteanimals/favourite_animals.py
@@ -7,3 +7,29 @@
 # One animal should be stored only at once
 # Each animal should be written in separate lines
 # The program should only save animals, no need to print them
+import sys
+
+class Controller(object):
+
+    def argument_reader(self):
+        print_help = Display()
+        add_animals = AddAnimal()
+        if len(sys.argv) == 1:
+            print_help.usage()
+        elif len(sys.argv) > 1:
+            animals = sys.argv[1:]            
+
+
+class Display(object):
+    def usage(self):
+        print("fav_animals [animal] [animal]")
+
+
+class AddAnimal(object):
+    def add_animal(self):
+        with open ("favourites.txt", "a") as file:
+            file.write(animals)
+
+
+run = Controller()
+run.argument_reader()
