0e69ed4ea0503334eb0a761c08f82ff816944476
Criollo01 <lucanagy@protonmail.com>
2017-10-06 12:38:26 +0200

    feat(unique_character): trying to solve it with dictionary of characters

--- uniquechars/unique_chars.py
+++ uniquechars/unique_chars.py
@@ -3,11 +3,25 @@
 # Create basic unit tests for it with at least 3 different test cases
 # Should print out:
 # ["n", "g", "r", "m"]
-
+'''
 def unique_characters(word):
-    unique = []
+    unique_character_list = []
     for char in word:
-        if char not in unique:
-            unique.append(char)
+        if char not in unique_character_list:
+            unique_character_list.append(char)
+    return unique_character_list
 
 print(unique_characters("anagram"))
+'''
+
+def unique_characters(string):
+    dict_of_chars = {}
+    for char in string:
+        keys = dict_of_chars.keys()
+        if char in keys:
+            dict_of_chars[char] += 1
+        else:
+            dict_of_chars[char] = 1
+    return dict_of_chars
+
+print(unique_characters('anagram'))
